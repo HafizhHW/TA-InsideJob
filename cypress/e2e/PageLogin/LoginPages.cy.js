2e1dd720d00bc017ad102b1dd7f0cc18ca4a144a
Hafizh Wijaya <hafizh.wijaya@brilife.co.id>
2025-06-29 21:46:10 +0700

    update 8

--- cypress/e2e/PageLogin/LoginPages.cy.js
+++ cypress/e2e/PageLogin/LoginPages.cy.js
@@ -3,30 +3,30 @@ import Login from "../../support/Object/PageLogin/LoginPages";
 
 const Log = new Login ();
 
-describe('Login Pages', () => {
-    beforeEach(() => {
-        cy.visit('https://www.saucedemo.com/')
-    })
-
-    it('Login', () => {
-        Log.aksiLogin();  // Lakukan login
-    })
-
-    it('Verifikasi btn Menu', () => {
-        // Pastikan untuk melakukan login sebelum memverifikasi
-        Log.aksiLogin();  // Lakukan login lagi
-        Log.klikMenu();   // Klik menu setelah login
-    })
-})
-
 // describe('Login Pages', () => {
 //     beforeEach(() => {
 //         cy.visit('https://www.saucedemo.com/')
 //     })
 
 //     it('Login', () => {
-//     Log.aksiLogin();
-//     Log.klikMenu();
+//         Log.aksiLogin();  // Lakukan login
 //     })
 
+//     it('Verifikasi btn Menu', () => {
+//         // Pastikan untuk melakukan login sebelum memverifikasi
+//         Log.aksiLogin();  // Lakukan login lagi
+//         Log.klikMenu();   // Klik menu setelah login
+//     })
 // })
+
+describe('Login Pages', () => {
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/')
+    })
+
+    it('Login', () => {
+    Log.aksiLogin();
+    Log.klikMenu();
+    })
+
+})
